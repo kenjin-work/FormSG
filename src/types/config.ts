39cadc897f04d08d83e9cccbd8dad97aa68abf5a
Ken <ken@open.gov.sg>
2023-03-08 16:53:05 +0800

    refactor: rename localDev to useMockTwilio

--- src/types/config.ts
+++ src/types/config.ts
@@ -78,7 +78,7 @@ export type Config = {
   // Consts
   isDev: boolean
   nodeEnv: Environment
-  isLocalDev: boolean
+  useMockTwilio: boolean
   port: number
   sessionSecret: string
   chromiumBin: string
@@ -152,7 +152,7 @@ export interface IOptionalVarsSchema {
     otpLifeSpan: number
     submissionsTopUp: number
     nodeEnv: Environment
-    localDev: boolean
+    useMockTwilio: boolean
   }
   banner: {
     isGeneralMaintenance: string
